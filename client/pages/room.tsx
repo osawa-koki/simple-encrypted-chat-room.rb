45020e35c37da078305d097236255b0c68c09289
osawa-koki <github@osawa.dev>
2023-03-21 21:14:56 +0900

    Current Room

--- client/pages/room.tsx
+++ client/pages/room.tsx
@@ -12,6 +12,27 @@ export default function RoomPage() {
   return (
     <Layout>
       <div id="Room">
+        <h1>🏡 Room</h1>
+        <hr />
+        <h2>Current Room</h2>
+        {
+          sharedData.current_room ? (
+            <Form className="mt-3">
+              <Form.Group>
+                <Form.Label>Room Name</Form.Label>
+                <Form.Control type="text" value={sharedData.current_room.room_name} readOnly />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Description</Form.Label>
+                <Form.Control type="text" value={sharedData.current_room.description} readOnly />
+              </Form.Group>
+            </Form>
+          ) : (
+            <Alert variant="warning" className="mt-3">
+              You are not in any room.
+            </Alert>
+          )
+        }
       </div>
     </Layout>
   );
