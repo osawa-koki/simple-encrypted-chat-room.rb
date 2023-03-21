5d40546287f675a9c104ddaeb917a3e4a81dcad8
osawa-koki <github@osawa.dev>
2023-03-21 18:19:07 +0900

    resources :rooms

--- server/app/controllers/api/rooms_controller.rb
+++ server/app/controllers/api/rooms_controller.rb
@@ -1,2 +1,29 @@
 class Api::RoomController < ApplicationController
+  def index
+    # GET /rooms
+  end
+
+  def show
+    # GET /rooms/:id
+  end
+
+  def new
+    # GET /rooms/new
+  end
+
+  def create
+    # POST /rooms
+  end
+
+  def edit
+    # GET /rooms/:id/edit
+  end
+
+  def update
+    # PATCH /rooms/:id
+  end
+
+  def destroy
+    # DELETE /rooms/:id
+  end
 end
