709a63cd9fff042dc9351be8d0a7ae068958ee21
osawa-koki <github@osawa.dev>
2023-03-21 19:19:46 +0900

    デモルの制約。

--- server/app/models/room.rb
+++ server/app/models/room.rb
@@ -1,2 +1,3 @@
 class Room < ApplicationRecord
+  validates :name, presence: true, uniqueness: true, length: { in: 3..16 }, format: { with: /\A[a-zA-Z0-9_-]+\z/, message: "must be alphanumeric and contain only '-', '_'" }
 end
