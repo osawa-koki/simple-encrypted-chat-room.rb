3acd2aa200e0ab1b457810dfd276e89727a0780c
osawa-koki <github@osawa.dev>
2023-03-23 22:32:55 +0900

    外部キー設定した。

--- server/db/migrate/20230323131336_create_chats.rb
+++ server/db/migrate/20230323131336_create_chats.rb
@@ -1,11 +1,12 @@
 class CreateChats < ActiveRecord::Migration[7.0]
   def change
     create_table :chats do |t|
-      t.integer :room_id, :null => false
+      t.references :room_id, :null => false, :foreign_key => true
       t.string :username, :null => false
       t.string :message, :null => false
 
       t.timestamps
     end
+    add_foreign_key :chats, :rooms, column: :id
   end
 end
