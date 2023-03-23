3ab3b0336e36701593b50abd776000f0db2c991f
osawa-koki <github@osawa.dev>
2023-03-23 22:26:26 +0900

    chats - index

--- server/app/controllers/api/chats_controller.rb
+++ server/app/controllers/api/chats_controller.rb
@@ -1,2 +1,18 @@
 class Api::ChatsController < ApplicationController
+  def index
+    # GET /api/chats
+    room_id = params[:room_id]
+    per_page = params[:per_page] || 30
+    since = params[:since] || Time.now.to_i
+
+    unless room_id
+      render json: { errors: ["Room ID is required."] }, status: :bad_request
+      return
+    end
+
+    @chats = Chat.all
+    @chats = @chats.where("room_id = ? AND created_at > ?", room_id.to_i, Time.at(since.to_i))
+    @chats = @chats.limit(per_page)
+    render json: @rooms
+  end
 end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :rooms
+    resources :chats
   end
 end
