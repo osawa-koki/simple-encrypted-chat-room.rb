db5b2426eaee97fdb6e74675c707c548422770fe
osawa-koki <github@osawa.dev>
2023-03-26 01:15:02 +0900

    警告の抑制。

--- server/config/environments/production.rb
+++ server/config/environments/production.rb
@@ -84,4 +84,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Disable automatic warning for production.
+  config.active_record.sqlite3_production_warning=false
 end
